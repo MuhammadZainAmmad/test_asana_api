87edbe75158dd29789715af3d6a216296b9b7835
MuhammadZainAmmad <zainammad05@gmail.com>
2023-07-18 19:57:12 +0500

    modified tasks detail code to only get the tasks which are modified

--- task1/asana_workspaceApiCall.py
+++ task1/asana_workspaceApiCall.py
@@ -3,6 +3,9 @@ import json
 import pandas as pd 
 import os
 
+from datetime import datetime
+import urllib.parse
+
 def asanaResToCSV(csvName, res):
     '''
         csvName: name of csv file in which data to dump
@@ -12,21 +15,21 @@ def asanaResToCSV(csvName, res):
     df = pd.DataFrame.from_dict(resDict['data'])
     if not os.path.exists('./fetchedRecords'):
         os.makedirs('./fetchedRecords')
-    df.to_csv(f'./fetchedRecords/{csvName}.csv')
+    df.to_csv(f'./fetchedRecords/{csvName}.csv', index= False)
 
 headers = {
     "accept": "application/json",
     "authorization": "Bearer 1/1204862582358995:58225b495568b45ac37d588db2fea9a6"
 }
 
-workspace = "https://app.asana.com/api/1.0/workspaces"
-resWorkspace = requests.get(workspace, headers=headers)
-print(resWorkspace.text) # type ==> 'dict' 
-asanaResToCSV('workspace', resWorkspace)
+# workspace = "https://app.asana.com/api/1.0/workspaces"
+# resWorkspace = requests.get(workspace, headers=headers)
+# # print(resWorkspace.text) # type ==> 'dict' 
+# asanaResToCSV('workspace', resWorkspace)
 
 projects = "https://app.asana.com/api/1.0/projects"
 resProjects = requests.get(projects, headers=headers)
-print((resProjects.text)) # type ==> 'dict' 
+# print((resProjects.text)) # type ==> 'dict' 
 asanaResToCSV('projects', resProjects)
 
 # Getting the project ID
@@ -34,41 +37,80 @@ resProjectsDict = json.loads(resProjects.text)
 projectID = resProjectsDict['data'][0]['gid']
 # print(projectID)
 
-section = f'https://app.asana.com/api/1.0/projects/{projectID}/sections'
-resSection = requests.get(section, headers=headers)
-print((resSection.text)) # type ==> 'dict' 
-asanaResToCSV('section', resSection)
+# section = f'https://app.asana.com/api/1.0/projects/{projectID}/sections'
+# resSection = requests.get(section, headers=headers)
+# # print((resSection.text)) # type ==> 'dict' 
+# asanaResToCSV('section', resSection)
 
-tasks = f"https://app.asana.com/api/1.0/tasks?project={projectID}"
-resTasks = requests.get(tasks, headers=headers)
-print((resTasks.text)) # type ==> 'dict' 
-asanaResToCSV('tasks', resTasks)
+# tasks = f"https://app.asana.com/api/1.0/tasks?project={projectID}"
+# resTasks = requests.get(tasks, headers=headers)
+# # print((resTasks.text)) # type ==> 'dict' 
+# asanaResToCSV('tasks', resTasks)
 
+if not os.path.exists('./fetchedRecords/tasks.csv'): # Logic for the first run of etl
+    tasks = f"https://app.asana.com/api/1.0/tasks?project={projectID}"
+    resTasks = requests.get(tasks, headers=headers)
+    # print((resTasks.text)) # type ==> 'dict'   
+    resDict = json.loads(resTasks.text) # String to dict
+    
+    df = pd.DataFrame.from_dict(resDict['data'])
+    
+    current_datetime = datetime.utcnow().isoformat()
+    encoded_datetime = urllib.parse.quote(current_datetime)
+    df['date_created'] = encoded_datetime
+    df['date_modified'] = encoded_datetime
+    print(current_datetime)
+    
+    if not os.path.exists('./fetchedRecords'):
+        os.makedirs('./fetchedRecords')
+        
+    df.to_csv(f'./fetchedRecords/tasks.csv', index=False)
+    print('file created')
+else:
+    print('file exists')
+    df_tasks = pd.read_csv('./fetchedRecords/tasks.csv')
+    print(df_tasks)
+    
+    last_modified = df_tasks['date_modified'].max()
+    
+    tasks = f"https://app.asana.com/api/1.0/tasks?project={projectID}&modified_since={last_modified}"
+    resTasks = requests.get(tasks, headers=headers)
+    # print((resTasks.text)) # type ==> 'dict'   
+    resDict = json.loads(resTasks.text) # String to dict
+    
+    df = pd.DataFrame.from_dict(resDict['data'])
+    
+    current_datetime = datetime.utcnow().isoformat()
+    encoded_datetime = urllib.parse.quote(current_datetime)
+    df['date_created'] = encoded_datetime
+    df['date_modified'] = encoded_datetime
+    
+    if not os.path.exists('./fetchedRecords'):
+        os.makedirs('./fetchedRecords')
+        
+    df.to_csv(f'./fetchedRecords/tasks_modified.csv', index=False)
+    print('modified file created')
 
+# resTasksDict = json.loads(resTasks.text)
+# tasksList = resTasksDict['data']
+# # print(tasksList)
 
+# def taskDetailsToCSV(name, id):
+#     task = f"https://app.asana.com/api/1.0/tasks/{id}"
+#     resTask = requests.get(task, headers=headers)
+#     # print((resTask.text)) # type ==> 'dict' 
+#     resDict = json.loads(resTask.text) # String to dict
+#     df = pd.json_normalize(resDict['data'], max_level=1)
+#     if not os.path.exists('./fetchedRecords/taskDetails'):
+#         os.makedirs('./fetchedRecords/taskDetails')
+#     df.to_csv(f'./fetchedRecords/taskDetails/{name}.csv', index = False)
 
-
-
-resTasksDict = json.loads(resTasks.text)
-tasksList = resTasksDict['data']
-# print(tasksList)
-
-def taskDetailsToCSV(name, id):
-    task = f"https://app.asana.com/api/1.0/tasks/{id}"
-    resTask = requests.get(task, headers=headers)
-    # print((resTask.text)) # type ==> 'dict' 
-    resDict = json.loads(resTask.text) # String to dict
-    df = pd.json_normalize(resDict['data'], max_level=1)
-    if not os.path.exists('./fetchedRecords/taskDetails'):
-        os.makedirs('./fetchedRecords/taskDetails')
-    df.to_csv(f'./fetchedRecords/taskDetails/{name}.csv')
-
-# Checking the id of each task in the multiple tasks response by asana
-count = 0
-for task in tasksList:
-    # print(task['gid'])
-    taskDetailsToCSV(f'task{count}', task['gid'])
-    count += 1
+# # Checking the id of each task in the multiple tasks response by asana
+# count = 0
+# for task in tasksList:
+#     # print(task['gid'])
+#     taskDetailsToCSV(f'task{count}', task['gid'])
+#     count += 1
 
 # Understanding the task response
 # resTaskDict = json.loads(resTasks.text)
