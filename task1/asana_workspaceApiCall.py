20b0ffb43c265f63e696917047866e7441a6567f
MuhammadZainAmmad <zainammad05@gmail.com>
2023-07-20 17:04:14 +0500

    simplified the logic

--- task1/asana_workspaceApiCall.py
+++ task1/asana_workspaceApiCall.py
@@ -51,48 +51,40 @@ projectID = resProjectsDict['data'][0]['gid']
 
 current_datetime = datetime.utcnow().isoformat()
 encoded_datetime = urllib.parse.quote(current_datetime)
-if not os.path.exists('./fetchedRecords/tasks.csv'): # Logic for the first run of etl
-    tasks = f"https://app.asana.com/api/1.0/tasks?project={projectID}"
-    resTasks = requests.get(tasks, headers=headers) 
-    resDict = json.loads(resTasks.text) # String to dict
-    df = pd.DataFrame.from_dict(resDict['data'])
-    
-    # Adding date_created and date_modified columns in fetched df
-    df['date_created'] = encoded_datetime
-    df['date_modified'] = encoded_datetime
-    
-    if not os.path.exists('./fetchedRecords'):
-        os.makedirs('./fetchedRecords')    
-    df.to_csv(f'./fetchedRecords/tasks.csv', index=False)
-    print('file created')
-else:
-    # using previous etl result
-    df_tasks = pd.read_csv('./fetchedRecords/tasks.csv')
+
+try: # file exists ==> some etl has already performed 
+    df_tasks = pd.read_csv('./fetchedRecords/tasks.csv') # Assumed that file is always present
     print(f'Previous df\n{df_tasks}\n')
+    last_modified = df_tasks['etl_date_modified'].max()
+except FileNotFoundError: # etl running for the first time 
+    df_tasks = pd.DataFrame(columns=['gid', 'name', 'resource_type', 'resource_subtype', 'etl_date_created', 'etl_date_modified'])
+    past_date = datetime(2000, 1, 1, 00, 00, 0000)
+    last_modified = past_date.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
+
+tasks = f"https://app.asana.com/api/1.0/tasks?project={projectID}&modified_since={last_modified}"
+resTasks = requests.get(tasks, headers=headers) 
+resDict = json.loads(resTasks.text) # String to dict
+df_modifiedTasks = pd.DataFrame.from_dict(resDict['data'])
+print(f'fetched df\n{df_modifiedTasks}\n')
     
-    last_modified = df_tasks['date_modified'].max()
-    
-    tasks = f"https://app.asana.com/api/1.0/tasks?project={projectID}&modified_since={last_modified}"
-    resTasks = requests.get(tasks, headers=headers) 
-    resDict = json.loads(resTasks.text) # String to dict
-    df_modifiedTasks = pd.DataFrame.from_dict(resDict['data'])
-    print(f'fetched df\n{df_modifiedTasks}\n')
-      
+if len(df_modifiedTasks) == 0: # No records are modifed 
+    print('No modified records!!!')    
+else:
     df_tasks['gid'] = df_tasks['gid'].astype(str)
-    
-    # Adding date_created and date_modified columns in fetched df
-    df_modifiedTasks['date_modified'] = encoded_datetime
-    df_modifiedTasks = df_modifiedTasks.merge(df_tasks[['gid', 'date_created']], on='gid', how='left')
-    df_modifiedTasks['date_created'].fillna(encoded_datetime, inplace=True)
+
+    # Adding etl_date_created and etl_date_modified columns in fetched df
+    df_modifiedTasks['etl_date_modified'] = encoded_datetime
+    df_modifiedTasks = df_modifiedTasks.merge(df_tasks[['gid', 'etl_date_created']], on='gid', how='left')
+    df_modifiedTasks['etl_date_created'].fillna(encoded_datetime, inplace=True)
     print(f'processed fetched df\n{df_modifiedTasks}\n')
-    
+
     # merging the two dfs
     df_merged = pd.concat([df_tasks, df_modifiedTasks]).drop_duplicates(subset='gid', keep='last')
     print(f'final df\n{df_merged}\n')
-    
+
     if not os.path.exists('./fetchedRecords'):
         os.makedirs('./fetchedRecords')        
-    df_merged.to_csv(f'./fetchedRecords/tasks_modified.csv', index=False)
+    df_merged.to_csv(f'./fetchedRecords/tasks.csv', index=False)
     print('modified file created')
 
 # resTasksDict = json.loads(resTasks.text)
